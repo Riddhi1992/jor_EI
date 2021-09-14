3dccb15c95d14950ac67a7d3105ef110c118553e
riddhi1992 <riddhipatel.csu@gmail.com>
2021-09-14 16:21:34 -0700

    FAQ page edited with Active/Nonactive Programs

--- sql_file/registered_user_table_contact_us.sql
+++ sql_file/registered_user_table_contact_us.sql
@@ -25,4 +25,9 @@ INSERT INTO `contact_us` (`id`, `name`, `email`, `emailSubject`, `emailBody`, `d
 (9, 'Ridhs', 'riddhipatel.csu@gmail.com', 'Hola', 'Testing...', '2021-08-27 15:09:41'),
 (10, 'Ridhs', 'riddhipatel.csu@gmail.com', 'Hola', 'How are you?', '2021-08-27 15:11:10'),
 (11, 'Ridhs', 'riddhipatel.csu@gmail.com', 'Hello', 'Testing email...', '2021-08-27 15:12:12'),
-(12, 'Ridhs', 'riddhipatel.csu@gmail.com', 'Hello', 'I think I got it...', '2021-08-27 15:24:15');
+(12, 'Ridhs', 'riddhipatel.csu@gmail.com', 'Hello', 'I think I got it...', '2021-08-27 15:24:15'),
+(39, 'Riddhi', 'riddhipatel.csu@gmail.com', 'Hello', 'Find the attachment', '2021-09-09 12:25:09'),
+(40, 'Riddhi', 'riddhipatel.csu@gmail.com', 'Hello', 'Find the attachments...', '2021-09-09 12:44:16'),
+(41, 'Riddhi', 'riddhipatel.csu@gmail.com', 'Hello', 'Find the attachments...', '2021-09-09 12:47:22'),
+(42, 'Riddhi', 'riddhipatel.csu@gmail.com', 'Hola', 'Please find the attached files...', '2021-09-09 12:48:29'),
+(43, 'Riddhi', 'riddhipatel.csu@gmail.com', 'Hello', 'Please find the attachments...', '2021-09-10 12:15:02');
--- sql_file/registered_user_table_registered_users.sql
+++ sql_file/registered_user_table_registered_users.sql
@@ -19,7 +19,6 @@ CREATE TABLE `registered_users` (
   `created` date NOT NULL DEFAULT current_timestamp(),
   `verification_code` varchar(255) NOT NULL,
   `is_verified` int(10) NOT NULL DEFAULT 0,
-  `status` int(50) NOT NULL DEFAULT 1,
   `resettoken` varchar(255) DEFAULT NULL,
   `resettokenexpire` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -28,9 +27,9 @@ CREATE TABLE `registered_users` (
 -- Dumping data for table `registered_users`
 --
 
-INSERT INTO `registered_users` (`id`, `first_name`, `last_name`, `company_name`, `user_name`, `email`, `password`, `re_password`, `business_type`, `user_type`, `created`, `verification_code`, `is_verified`, `status`, `resettoken`, `resettokenexpire`) VALUES
-(9, 'User', 'user02', '', 'user_01', 'patelriddhi01992@gmail.com', '$2y$10$VwHjYS01MBMGSpxV1xrlGeAZk7TMqXtYGPVv8NbIE8ZXUmWkHtevy', '$2y$10$ITA8/BJ2Suw.ccRCgu7/o.smUG/GMC37dFOXdBXP6RnkXveIka8PS', '', 'User', '2021-08-05', '434637e3599b91a06b2295c159130e73', 1, 1, NULL, NULL),
-(10, 'Owner', 'owner', 'ABC', 'owner_01', 'riddhi@drhologram.com', '$2y$10$VKN0WSsKSPzh5vJtFFCjFOuvpXkg7WvK0jDqt24CHQPs82Zl5uUbG', '$2y$10$DetNy.J9kn7MdT337lN9qubEWtLAgfUdvhG8vIJMWDQvWJHmV.A1O', 'Restaurant', 'Owner', '2021-08-05', '56ec3523a87453781e01bf7dd0f75e2f', 1, 1, NULL, NULL),
-(12, 'User2', 'user2', '', 'user_02', 'r.patel.241@westcliff.edu', 'user_02', 'user_02', '', 'User', '2021-08-05', '175479b9d8519cee9b72e94fa854582f', 0, 0, NULL, NULL),
-(13, 'Admin_02', 'admin', '', 'admin_02', 'admin@gmail.com', 'admin_02', 'admin_02', '', 'Admin', '2021-08-06', 'b54cd52e5fbe6a525b94071270473c28', 0, 0, NULL, NULL),
-(14, 'Admin_01', 'admin', '', 'admin_01', 'riddhipatel.csu@gmail.com', '$2y$10$dhE9.FMxf0RR3PirXCLUsuTT6YQa10sHOL2Az7V/qGxESizVV4clG', '$2y$10$gzdKu1SDd9zevpfLseowD.9rRBOhUBSELwNKmJB3jn6zWC5kSF6nS', '', 'Admin', '2021-08-23', 'fc72e06dd354284dd29e57d26f43e301', 1, 1, '7cf32269c5e420a31d6ca058b17ff3b0', '2021-08-25');
+INSERT INTO `registered_users` (`id`, `first_name`, `last_name`, `company_name`, `user_name`, `email`, `password`, `re_password`, `business_type`, `user_type`, `created`, `verification_code`, `is_verified`, `resettoken`, `resettokenexpire`) VALUES
+(9, 'User', 'user02', '', 'user_01', 'patelriddhi01992@gmail.com', '1234', '1234', '', 'User', '2021-08-05', '434637e3599b91a06b2295c159130e73', 1, NULL, NULL),
+(10, 'Owner', 'owner', 'ABC', 'owner_01', 'riddhi@drhologram.com', '12345', '12345', 'Restaurant', 'Owner', '2021-08-05', '56ec3523a87453781e01bf7dd0f75e2f', 1, NULL, NULL),
+(12, 'User2', 'user2', '', 'user_02', 'r.patel.241@westcliff.edu', 'user_02', 'user_02', '', 'User', '2021-08-05', '175479b9d8519cee9b72e94fa854582f', 0, NULL, NULL),
+(13, 'Admin_02', 'admin', '', 'admin_02', 'admin@gmail.com', 'admin_02', 'admin_02', '', 'Admin', '2021-08-06', 'b54cd52e5fbe6a525b94071270473c28', 0, NULL, NULL),
+(20, 'owner02', 'owner', 'XYZ', 'Owner02', 'riddhipatel.csu@gmail.com', '123456', '123456', 'Restaurant', 'Owner', '2021-09-09', '1086edcd32c82c9cabd3d411a97524eb', 1, NULL, NULL);
